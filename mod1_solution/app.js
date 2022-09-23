c0c32324d619520e4dd1e417865d8c01706cd9b8
tskauf <tskauf@gmail.com>
2022-09-23 11:56:19 -0500

    Final, with handling for empy strings.

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -1,23 +1,35 @@
 (function(){
-'use strict'
+    'use strict'
+    
+    angular.module('LunchCheck',[])
+    .controller('LunchCheckController', LunchCheckController);
+    
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.message = '';
+        $scope.foodList = '';
+    
+        $scope.checkMessage = function() {
+            var chkr = $scope.foodList
+            var msg = ''
+            
+            //Turn results into an array, split by comma
+            chkr = chkr.split(',')
 
-angular.module('LunchCheck',[])
-.controller('LunchCheckController', LunchCheckController);
+            //Trim all additional spaces from array
+            chkr = chkr.map(e => {return e.trim();});
+            
+            //Check array and remove empty strings
+            chkr = chkr.filter(e => {return e !== '';});
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
-    $scope.message = '';
-    $scope.foodList = '';
+            //Check to see if you ate too much lunch
+            if (chkr.length == 0) {msg = "Please enter data first."}
+            else if(chkr.length > 3) {msg = "Too much!"}
+            else {msg = "Enjoy!"};
 
-    $scope.checkMessage = function() {
-        var chkr = $scope.foodList
-        if (chkr == "") return $scope.message = "Please enter data first.";
+            $scope.message = msg
 
-        chkr = chkr.split(',')
-
-        if(chkr.length > 3) return $scope.message = "Too much!"
-        else return $scope.message = "Enjoy!"
+        };
     };
-};
-
-})();
+    
+    })();
